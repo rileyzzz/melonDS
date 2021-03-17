5af4473519d8466e05d0c494fb9536e09cd0543e
rileyzzz <rileylemmler@gmail.com>
2021-03-17 17:36:46 +0000

    stuff 3

--- src/frontend/emscripten/Platform.cpp
+++ src/frontend/emscripten/Platform.cpp
@@ -7,7 +7,11 @@
 #include <sys/select.h>
 #include <sys/socket.h>
 
+#include <fstream>
 #include <filesystem>
+#include <thread>
+#include <mutex>
+//#include <semaphore>
 
 #define socket_t    int
 #define sockaddr_t  struct sockaddr
@@ -24,6 +28,7 @@ char* EmuDirectory;
 
 void emuStop();
 
+#define PLATFORM_SEPARATOR std::filesystem::path::preferred_separator
 
 namespace Platform
 {
@@ -36,7 +41,8 @@ u8 PacketBuffer[2048];
 
 void Init(int argc, char** argv)
 {
-    EmuDirectory = "/";
+    strcpy(EmuDirectory, "/");
+
 }
 
 void DeInit()
@@ -57,36 +63,297 @@ FILE* OpenFile(const char* path, const char* mode, bool mustexist)
         return nullptr;
     }
 
-    QFile f(path);
+    // std::ios::openmode smode;
+    // if (strlen(mode) > 1 && mode[0] == 'r' && mode[1] == '+')
+    //     smode = std::ios::in | std::ios::out;
+    // else if (strlen(mode) > 1 && mode[0] == 'w' && mode[1] == '+')
+    //     smode = std::ios::trunc | std::ios::in | std::ios::out;
+    // else if (mode[0] == 'w')
+    //     smode = std::ios::trunc | std::ios::out;
+    // else
+    //     smode = std::ios::in;
+    
+    // std::ifstream str(f, smode);
+    // FILE* file = fdopen(dup(str.rdbuf()->fd(), mode);
+    // str.close();
+    FILE* file = fopen(path, mode);
 
-    if (mustexist && !f.exists())
-    {
-        return nullptr;
-    }
+    return file;
+}
 
-    QIODevice::OpenMode qmode;
-    if (strlen(mode) > 1 && mode[0] == 'r' && mode[1] == '+')
+FILE* OpenLocalFile(const char* path, const char* mode)
+{
+    std::filesystem::path dir(path);
+    std::string fullpath;
+
+    if (dir.is_absolute())
     {
-		qmode = QIODevice::OpenModeFlag::ReadWrite;
-	}
-	else if (strlen(mode) > 1 && mode[0] == 'w' && mode[1] == '+')
-    {
-    	qmode = QIODevice::OpenModeFlag::Truncate | QIODevice::OpenModeFlag::ReadWrite;
-	}
-	else if (mode[0] == 'w')
-    {
-        qmode = QIODevice::OpenModeFlag::Truncate | QIODevice::OpenModeFlag::WriteOnly;
+        // If it's an absolute path, just open that.
+        fullpath = path;
     }
     else
     {
-        qmode = QIODevice::OpenModeFlag::ReadOnly;
+        fullpath = std::string(EmuDirectory) + PLATFORM_SEPARATOR + path;
     }
 
-    f.open(qmode);
-    FILE* file = fdopen(dup(f.handle()), mode);
-    f.close();
+    return OpenFile(fullpath.c_str(), mode, mode[0] != 'w');
+}
 
-    return file;
+Thread* Thread_Create(std::function<void()> func)
+{
+    std::thread* t = new std::thread(func);
+    //QThread* t = QThread::create(func);
+    //t->start();
+    return (Thread*) t;
+}
+
+void Thread_Free(Thread* thread)
+{
+    std::thread* t = (std::thread*) thread;
+    //t->terminate();
+    delete t;
+}
+
+void Thread_Wait(Thread* thread)
+{
+    ((std::thread*) thread)->join();
+}
+
+// Semaphore* Semaphore_Create()
+// {
+//     return (Semaphore*)new std::counting_semaphore();
+// }
+
+// void Semaphore_Free(Semaphore* sema)
+// {
+//     delete (std::counting_semaphore*) sema;
+// }
+
+// void Semaphore_Reset(Semaphore* sema)
+// {
+//     std::counting_semaphore* s = (std::counting_semaphore*) sema;
+
+//     s->acquire(s->available());
+// }
+
+// void Semaphore_Wait(Semaphore* sema)
+// {
+//     ((std::counting_semaphore*) sema)->acquire();
+// }
+
+// void Semaphore_Post(Semaphore* sema, int count)
+// {
+//     ((std::counting_semaphore*) sema)->release(count);
+// }
+
+Mutex* Mutex_Create()
+{
+    return (Mutex*)new std::mutex();
+}
+
+void Mutex_Free(Mutex* mutex)
+{
+    delete (std::mutex*) mutex;
+}
+
+void Mutex_Lock(Mutex* mutex)
+{
+    ((std::mutex*) mutex)->lock();
+}
+
+void Mutex_Unlock(Mutex* mutex)
+{
+    ((std::mutex*) mutex)->unlock();
+}
+
+bool Mutex_TryLock(Mutex* mutex)
+{
+    return ((std::mutex*) mutex)->try_lock();
+}
+
+
+bool MP_Init()
+{
+    int opt_true = 1;
+    int res;
+
+#ifdef __WIN32__
+    WSADATA wsadata;
+    if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0)
+    {
+        return false;
+    }
+#endif // __WIN32__
+
+    MPSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (MPSocket < 0)
+    {
+        return false;
+    }
+
+    res = setsockopt(MPSocket, SOL_SOCKET, SO_REUSEADDR, (const char*)&opt_true, sizeof(int));
+    if (res < 0)
+    {
+        closesocket(MPSocket);
+        MPSocket = INVALID_SOCKET;
+        return false;
+    }
+
+    sockaddr_t saddr;
+    saddr.sa_family = AF_INET;
+    *(u32*)&saddr.sa_data[2] = htonl(Config::SocketBindAnyAddr ? INADDR_ANY : INADDR_LOOPBACK);
+    *(u16*)&saddr.sa_data[0] = htons(7064);
+    res = bind(MPSocket, &saddr, sizeof(sockaddr_t));
+    if (res < 0)
+    {
+        closesocket(MPSocket);
+        MPSocket = INVALID_SOCKET;
+        return false;
+    }
+
+    res = setsockopt(MPSocket, SOL_SOCKET, SO_BROADCAST, (const char*)&opt_true, sizeof(int));
+    if (res < 0)
+    {
+        closesocket(MPSocket);
+        MPSocket = INVALID_SOCKET;
+        return false;
+    }
+
+    MPSendAddr.sa_family = AF_INET;
+    *(u32*)&MPSendAddr.sa_data[2] = htonl(INADDR_BROADCAST);
+    *(u16*)&MPSendAddr.sa_data[0] = htons(7064);
+
+    return true;
+}
+
+void MP_DeInit()
+{
+    if (MPSocket >= 0)
+        closesocket(MPSocket);
+
+#ifdef __WIN32__
+    WSACleanup();
+#endif // __WIN32__
+}
+
+int MP_SendPacket(u8* data, int len)
+{
+    if (MPSocket < 0)
+        return 0;
+
+    if (len > 2048-8)
+    {
+        printf("MP_SendPacket: error: packet too long (%d)\n", len);
+        return 0;
+    }
+
+    *(u32*)&PacketBuffer[0] = htonl(0x4946494E); // NIFI
+    PacketBuffer[4] = NIFI_VER;
+    PacketBuffer[5] = 0;
+    *(u16*)&PacketBuffer[6] = htons(len);
+    memcpy(&PacketBuffer[8], data, len);
+
+    int slen = sendto(MPSocket, (const char*)PacketBuffer, len+8, 0, &MPSendAddr, sizeof(sockaddr_t));
+    if (slen < 8) return 0;
+    return slen - 8;
+}
+
+int MP_RecvPacket(u8* data, bool block)
+{
+    if (MPSocket < 0)
+        return 0;
+
+    fd_set fd;
+    struct timeval tv;
+
+    FD_ZERO(&fd);
+    FD_SET(MPSocket, &fd);
+    tv.tv_sec = 0;
+    tv.tv_usec = block ? 5000 : 0;
+
+    if (!select(MPSocket+1, &fd, 0, 0, &tv))
+    {
+        return 0;
+    }
+
+    sockaddr_t fromAddr;
+    socklen_t fromLen = sizeof(sockaddr_t);
+    int rlen = recvfrom(MPSocket, (char*)PacketBuffer, 2048, 0, &fromAddr, &fromLen);
+    if (rlen < 8+24)
+    {
+        return 0;
+    }
+    rlen -= 8;
+
+    if (ntohl(*(u32*)&PacketBuffer[0]) != 0x4946494E)
+    {
+        return 0;
+    }
+
+    if (PacketBuffer[4] != NIFI_VER)
+    {
+        return 0;
+    }
+
+    if (ntohs(*(u16*)&PacketBuffer[6]) != rlen)
+    {
+        return 0;
+    }
+
+    memcpy(data, &PacketBuffer[8], rlen);
+    return rlen;
+}
+
+
+
+bool LAN_Init()
+{
+    throw "LAN NOT SUPPORTED";
+    return false;
+    // if (Config::DirectLAN)
+    // {
+    //     if (!LAN_PCap::Init(true))
+    //         return false;
+    // }
+    // else
+    // {
+    //     if (!LAN_Socket::Init())
+    //         return false;
+    // }
+
+    // return true;
+}
+
+void LAN_DeInit()
+{
+    throw "LAN NOT SUPPORTED";
+    // LAN_PCap::DeInit();
+    // LAN_Socket::DeInit();
+}
+
+int LAN_SendPacket(u8* data, int len)
+{
+    throw "LAN NOT SUPPORTED";
+    // if (Config::DirectLAN)
+    //     return LAN_PCap::SendPacket(data, len);
+    // else
+    //     return LAN_Socket::SendPacket(data, len);
+}
+
+int LAN_RecvPacket(u8* data)
+{
+    throw "LAN NOT SUPPORTED";
+    // if (Config::DirectLAN)
+    //     return LAN_PCap::RecvPacket(data);
+    // else
+    //     return LAN_Socket::RecvPacket(data);
+}
+
+void Sleep(u64 usecs)
+{
+    //microseconds
+    //QThread::usleep(usecs);
+    std::this_thread::sleep_for(std::chrono::microseconds(usecs));
 }
 
 }
